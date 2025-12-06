5f7adc04a66ac9f00ce6238c90dbc02bb5f831b9
Ejay Kazumi <144519604+ejaykazumi0309@users.noreply.github.com>
2025-12-06 12:10:44 +0800

    Update Student.java

--- src/dorsu/edu/ph/Student.java
+++ src/dorsu/edu/ph/Student.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package dorsu.edu.ph;
 
-/**
- *
- * @author ejay
- */
 import java.io.Serializable;
 public class Student extends Person implements Serializable{
     int id;
